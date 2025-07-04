1e477333b63332759b74faee79a1c4ce59b901e6
Nibour16 <ruobinh3@hotmail.com>
2025-07-03 21:33:25 -0600

    Create ScriptableObject_Fossil

---
+++ Assets/Robin/Scripts/Fossil/BaseCollectible.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+public abstract class BaseCollectible : ScriptableObject
+{
+    public string collectibleName;
+    public float collectibleValue;
+}
---
+++ Assets/Robin/Scripts/Fossil/Fossil.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewFossil", menuName = "ScriptableObjects/Fossil", order = 1)]
+public class Fossil : BaseCollectible
+{
+    public DinosaurType fossilType;
+    
+}
