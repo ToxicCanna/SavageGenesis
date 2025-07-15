7efd52df2f2bfe539eaa2a32afa1d500032b94fc
Nibour16 <ruobinh3@hotmail.com>
2025-07-15 16:39:23 -0600

    Using William's base state machine and dinosaur type enum

--- Assets/Common/Robin/StateMachineUtilities/BaseState.cs
+++
@@ -1,6 +0,0 @@
-public abstract class BaseState
-{
-    public abstract void EnterState();
-    public abstract void UpdateState();
-    public abstract void ExitState();
-}
--- Assets/Robin/Scripts/Enumeration/GameEnumerator.cs
+++
@@ -1,6 +0,0 @@
-public enum DinosaurType
-{
-    Carnivore,
-    Herbivore,
-    Aerial
-}
