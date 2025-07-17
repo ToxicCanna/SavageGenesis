f7c9972339a0c098c12cdcc64946bebce7b79feb
ToxicCanna <freedom22140@gmail.com>
2025-07-17 07:23:52 -0600

    Prototype Scene Return
    -Digging area boundary adjustment
    -Undergroud and stadium proto scenes

---
+++ Assets/SceneReturn.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneReturn : MonoBehaviour
+{
+    public void LoadScene()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
