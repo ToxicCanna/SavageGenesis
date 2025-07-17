e661b396d0d4c1dc9b7614081ea85348d2cc4a1b
Nibour16 <ruobinh3@hotmail.com>
2025-07-16 18:31:18 -0600

    Digging Function Working progress

--- Assets/Robin/FossilArea/DiggingLayer.cs
+++ Assets/Robin/FossilArea/DiggingLayer.cs
@@ -5,5 +5,10 @@ public class DiggingLayer : BaseMiningLayer, IDiggingArea
     public void OnDigging(Vector2 diggingPos, DiggingToolType tool)
     {
         Debug.Log($"Trigger Point: {diggingPos}; Tool Using: {tool}");
+
+        if (tilemap != null)
+            tilemap.SetTile(new Vector3Int((int)diggingPos.x, (int)diggingPos.y, 0), null);
+        else
+            Debug.Log("Tilemap is not found!");
     }
 }
--- Assets/Robin/Scripts/Utilities/BaseMiningLayer.cs
+++ Assets/Robin/Scripts/Utilities/BaseMiningLayer.cs
@@ -1,6 +1,12 @@
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public abstract class BaseMiningLayer : MonoBehaviour
 {
-    
+    protected Tilemap tilemap;
+
+    protected virtual void Start()
+    {
+        tilemap = GetComponentInChildren<Tilemap>();
+    }
 }
